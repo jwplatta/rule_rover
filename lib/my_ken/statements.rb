503e315eb3265c914d43eb945fe42200a6b7cf46
Joseph Platta <jwplatta@gmail.com>
2021-06-28 23:51:59 -0700

    Add a Statement class that will eventually replace all the Structs. Also refactor the conjunctive normal form logic to operate on objects of the new Statement class

--- lib/my_ken/statements.rb
+++ lib/my_ken/statements.rb
@@ -1,5 +1,217 @@
 module MyKen
   module Statements
+    VALID_CONSTANTS = /A-Z/
+    VALID_OPERATORS = /⊃|≡|not|or$|and$/
+    VALID_VARIABLES = /a-z/
+
+    def substitute(); end
+    def unify(); end
+
+    def self.to_conjunctive_normal_form(statement)
+      raise ArgumentError.new("statement is not an instance of #{Statement.class}") unless statement.is_a? Statement
+
+      # STEP: eliminate biconditionals
+      statement = eliminate_biconditionals(statement)
+      # STEP: eliminate conditionals
+      statement = eliminate_conditionals(statement)
+      # STEP: move_negation_to_literals
+      statement = move_negation_to_literals(statement)
+      # STEP: distribute AND over OR
+      distribute(statement)
+    end
+
+    def self.eliminate_biconditionals(statement)
+      if statement.statements.empty?
+        statement
+      elsif statement.statements.size == 1
+        Statement.new(
+          statement.identifier,
+          eliminate_biconditionals(statement.statements[0])
+        )
+      elsif statement.identifier == "≡"
+        cond_x = eliminate_biconditionals(statement.statements[0])
+        cond_y = eliminate_biconditionals(statement.statements[1])
+
+        Statement.new(
+          "and",
+          Statement.new(
+            "⊃",
+            cond_x,
+            cond_y
+          ),
+          Statement.new(
+            "⊃",
+            cond_y,
+            cond_x
+          )
+        )
+      else
+        Statement.new(
+          statement.identifier,
+          eliminate_biconditionals(statement.statements[0]),
+          eliminate_biconditionals(statement.statements[1])
+        )
+      end
+    end
+
+    def self.eliminate_conditionals(statement)
+      if statement.statements.empty?
+        statement
+      elsif statement.statements.size == 1
+        Statement.new(
+          statement.identifier,
+          eliminate_conditionals(statement.statements[0])
+        )
+      elsif statement.identifier == "⊃"
+        Statement.new(
+          "not",
+          eliminate_conditionals(statement.statements[0])
+        ).then do |antecedent|
+          Statement.new(
+            "or",
+            antecedent,
+            eliminate_conditionals(statement.statements[1])
+          )
+        end
+      else
+        Statement.new(
+          statement.identifier,
+          eliminate_conditionals(statement.statements[0]),
+          eliminate_conditionals(statement.statements[1])
+        )
+      end
+    end
+
+    def self.move_negation_to_literals(statement)
+      if statement.statements.empty?
+        statement
+      elsif statement.identifier == "not"
+        negated_statement = statement.statements[0]
+        if negated_statement.statements.empty?
+          statement
+        elsif negated_statement.identifier == "not"
+          move_negation_to_literals(negated_statement.statements[0])
+        elsif negated_statement.identifier == "or"
+          Statement.new(
+            "and",
+            negated_statement.statements[0].not,
+            negated_statement.statements[1].not
+          ).then { |stmt| move_negation_to_literals(stmt) }
+        elsif negated_statement.identifier == "and"
+          Statement.new(
+            "or",
+            negated_statement.statements[0].not,
+            negated_statement.statements[1].not
+          ).then { |stmt| move_negation_to_literals(stmt) }
+        end
+      else
+        Statement.new(
+          statement.identifier,
+          move_negation_to_literals(statement.statements[0]),
+          move_negation_to_literals(statement.statements[1])
+        )
+      end
+    end
+
+    def self.distribute(statement)
+      while distribute_any?(statement)
+        statement = and_over_or(statement)
+      end
+
+      statement
+    end
+
+    def self.and_over_or(statement)
+      # NOTE: (alpha or (beta and gamma)) :: ((alpha or beta) and (alpha or gamma))
+      # (A or (B and C))
+      #
+      # (A or ((B and C) or D))
+      # (A or ((B or D) and (C or D)))
+      # ((A or (B or D)) and (A or (C or D)))
+      #
+      # ((B and C) or (A and D))
+      # ((B and C) or A) and ((B and C) or D)
+      if statement.statements.empty? or statement.identifier == "not"
+        statement
+      elsif statement.identifier == "or" and statement.statements[0].identifier == "and"
+        lhs = Statement.new("or", statement.statements[0].statements[0], statement.statements[1])
+        rhs = Statement.new("or", statement.statements[0].statements[1], statement.statements[1])
+        Statement.new("and", lhs, rhs)
+      elsif statement.identifier == "or" and statement.statements[1].identifier == "and"
+        lhs = Statement.new("or", statement.statements[0], statement.statements[1].statements[0])
+        rhs = Statement.new("or", statement.statements[0], statement.statements[1].statements[1])
+        Statement.new("and", lhs, rhs)
+      else
+        Statement.new(
+          statement.identifier,
+          and_over_or(statement.statements[0]),
+          and_over_or(statement.statements[1])
+        )
+      end
+    end
+
+    def self.distribute_any?(statement)
+      if statement.statements.empty?
+        false
+      elsif statement.identifier == "not"
+        distribute_any?(statement.statements[0])
+      elsif statement.identifier == "and"
+        distribute_any?(statement.statements[0]) or \
+        distribute_any?(statement.statements[1])
+      elsif statement.identifier == "or"
+        statement.statements[0].identifier == "and" or \
+        statement.statements[1].identifier == "and" or \
+        distribute_any?(statement.statements[0]) or \
+        distribute_any?(statement.statements[1])
+      end
+    end
+
+    class Statement
+      attr_reader :identifier, :statements
+
+      def initialize(identifier, *statements)
+        raise ArgumentError.new("Not a valid statement") if identifier.match(VALID_OPERATORS) and statements.empty?
+
+        @identifier = identifier
+        @statements = statements
+      end
+
+      def not
+        Statement.new("not", self)
+      end
+
+      def and(other)
+        Statement.new("and", self, other)
+      end
+
+      def or(other)
+        Statement.new("or", self, other)
+      end
+
+      def ⊃(other)
+        Statement.new("⊃", self, other)
+      end
+
+      def ≡(other)
+        Statement.new("≡", self, other)
+      end
+
+      def to_s
+        if statements.size > 1
+          statements.map(&:to_s).join(" #{identifier} ").then { |stmts| "(#{stmts})" }
+        elsif statements.size == 1
+          stmt = statements[0].to_s
+          if stmt[0] == "(" and stmt[-1] == ")"
+            "#{identifier}#{stmt}"
+          else
+            "#{identifier}(#{stmt})"
+          end
+        else
+          "#{identifier}"
+        end
+      end
+    end
+
     class NullStatement
       def atomic?
         false
--- spec/statements_spec.rb
+++ spec/statements_spec.rb
@@ -1,6 +1,158 @@
 require 'spec_helper'
 
 describe MyKen::Statements do
+  describe 'Statement class' do
+    context 'atomic statements' do
+      it do
+        aggregate_failures do
+          expect(MyKen::Statements::Statement.new("A").to_s).to eq "A"
+          expect do
+            MyKen::Statements::Statement.new("and")
+          end.to raise_error(ArgumentError)
+        end
+      end
+    end
+    context 'complex statements' do
+      it do
+        aggregate_failures do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          c = MyKen::Statements::Statement.new("C")
+          d = MyKen::Statements::Statement.new("D")
+
+          not_d = d.not
+          a_and_b = a.and(b)
+          c_or_a_and_b = c.or(a_and_b)
+          not_d_cond_c_or_a_and_b = not_d.⊃(c_or_a_and_b)
+          c_bicond_d = c.≡(d)
+
+          expect(not_d.to_s).to eq "not(D)"
+          expect(a_and_b.to_s).to eq "(A and B)"
+          expect(c_or_a_and_b.to_s).to eq "(C or (A and B))"
+          expect(not_d_cond_c_or_a_and_b.to_s).to eq "(not(D) ⊃ (C or (A and B)))"
+          expect(c_bicond_d.to_s).to eq "(C ≡ D)"
+        end
+      end
+    end
+    describe 'conjunctive normal form' do
+      context 'complex statements' do
+        context '.to_conjunctive_normal_form' do
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            d = MyKen::Statements::Statement.new("D")
+
+            not_a_or_b_or_c_bicond_d = a.or(b).not.or(c.≡(d))
+            # "(not(A or B) or (C ≡ D))"
+            # (not(A) and not(B)) or ((C ⊃ D) and (D ⊃ C))
+            # (not(A) and not(B)) or ((not(C) or D) and (not(D) or C))
+            # "((not(A) and not(B)) or ((not(C) or D) and (not(D) or C)))"
+            # (not(A) or ((not(C) or D) and (not(D) or C))) and (not(B) or ((not(C) or D) and (not(D) or C)))
+            # (not(A) or (not(C) or D)) and (not(A) or (not(D) or C)) and (not(B) or (not(C) or D)) and (not(B) or (not(D) or C))
+
+            result = MyKen::Statements.to_conjunctive_normal_form(not_a_or_b_or_c_bicond_d)
+
+            expect(result.to_s).to eq("(((not(A) or (not(C) or D)) and (not(A) or (not(D) or C))) and ((not(B) or (not(C) or D)) and (not(B) or (not(D) or C))))")
+          end
+        end
+        context '.eliminates_biconditionals' do
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            b_bicond_c = b.≡(c)
+            a_bicond_b = a.≡(b_bicond_c)
+            result = MyKen::Statements.eliminate_biconditionals(a_bicond_b)
+            expect(result.to_s).to eq("((A ⊃ ((B ⊃ C) and (C ⊃ B))) and (((B ⊃ C) and (C ⊃ B)) ⊃ A))")
+          end
+        end
+        context '.eliminates_conditionals' do
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            b_cond_c = b.⊃(c)
+            a_cond_b = a.⊃(b_cond_c)
+
+            result = MyKen::Statements.eliminate_conditionals(a_cond_b)
+            expect(result.to_s).to eq("(not(A) or (not(B) or C))")
+          end
+        end
+        context '.move_negation_to_literals' do
+          it 'eliminates double negation of atomic' do
+            not_not_a = MyKen::Statements::Statement.new("A").not.not
+            result = MyKen::Statements.move_negation_to_literals(not_not_a)
+            expect(result.to_s).to eq "A"
+          end
+          it 'eliminates double negation of complex statement' do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            not_not_a_and_b = a.and(b).not.not
+            result = MyKen::Statements.move_negation_to_literals(not_not_a_and_b)
+            expect(result.to_s).to eq "(A and B)"
+          end
+          it do
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            b_or_c = b.or(c)
+            not_b_or_c = b_or_c.not
+            result = MyKen::Statements.move_negation_to_literals(not_b_or_c)
+            expect(result.to_s).to eq("(not(B) and not(C))")
+          end
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            not_a = a.not
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            b_or_c = b.or(c)
+            not_b_or_c = b_or_c.not
+            not_not_b_or_c_and_not_a = not_b_or_c.and(not_a).not
+            result = MyKen::Statements.move_negation_to_literals(not_not_b_or_c_and_not_a)
+            expect(result.to_s).to eq("((B or C) or A)")
+          end
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            not_a = a.not
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            b_or_c = b.or(c)
+            not_b_or_c_and_not_a = b_or_c.and(not_a).not
+            result = MyKen::Statements.move_negation_to_literals(not_b_or_c_and_not_a)
+            expect(result.to_s).to eq("((not(B) and not(C)) or A)")
+          end
+        end
+        context '.distribute' do
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            a_or_b_and_c = a.or(b.and(c))
+            result = MyKen::Statements.distribute(a_or_b_and_c)
+            expect(result.to_s).to eq("((A or B) and (A or C))")
+          end
+          it do
+            a = MyKen::Statements::Statement.new("A")
+            b = MyKen::Statements::Statement.new("B")
+            c = MyKen::Statements::Statement.new("C")
+            d = MyKen::Statements::Statement.new("D")
+            a_and_d_or_b_and_c = a.and(d).or(b.and(c))
+            result = MyKen::Statements.distribute(a_and_d_or_b_and_c)
+            # "((A and D) or (B and C))"
+            # (A or (B and C)) and (D or (B and C))
+            # (((A or B) and (A or C)) and ((D or B) and (D or C)))
+            # "(((A or B) and (A or C)) and ((D or B) and (D or C)))"
+            expect(result.to_s).to eq("(((A or B) and (A or C)) and ((D or B) and (D or C)))")
+          end
+        end
+      end
+    end
+  end
+
+  #####################################
+  ### OG Statements Data Structures ###
+  #####################################
+
   context 'complex statements' do
     context 'when conjunction or disjunction operator' do
       it 'return correct value' do
