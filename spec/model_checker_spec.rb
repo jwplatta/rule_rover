7b2a589bcea675345924cfdbe803996260068c55
Joseph Platta <jwplatta@gmail.com>
2021-07-04 12:52:38 -0700

    implement an .entail? method in the ModelChecker module that applies model checking to a propositional knowledge base

--- spec/model_checker_spec.rb
+++ spec/model_checker_spec.rb
@@ -1,6 +1,130 @@
 require 'spec_helper'
 
 describe MyKen::ModelChecker do
+  describe '.literals' do
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      b = MyKen::Statements::Statement.new("B")
+      c = MyKen::Statements::Statement.new("C")
+      d = MyKen::Statements::Statement.new("D")
+
+      literals = MyKen::ModelChecker.literals(a.and(b).or(c.not).and(d).and(a.not).or(b))
+      expect(literals.count).to eq(4)
+    end
+  end
+  describe '.true_in_model?' do
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      model = {
+        "A" => true
+      }
+
+      result = MyKen::ModelChecker.true_in_model?(a, model)
+      expect(result).to be true
+    end
+    context 'negation statement' do
+      it do
+        a = MyKen::Statements::Statement.new("A")
+        model = {
+          "A" => true
+        }
+
+        result = MyKen::ModelChecker.true_in_model?(a.not, model)
+        expect(result).to be false
+      end
+    end
+    context 'conjunction statement' do
+      it do
+        a = MyKen::Statements::Statement.new("A")
+        b = MyKen::Statements::Statement.new("B")
+        model = {
+          "A" => true,
+          "B" => false
+        }
+
+        result = MyKen::ModelChecker.true_in_model?(a.and(b), model)
+        expect(result).to be false
+      end
+    end
+    context 'disjunction statement' do
+      it do
+        a = MyKen::Statements::Statement.new("A")
+        b = MyKen::Statements::Statement.new("B")
+        model = {
+          "A" => true,
+          "B" => false
+        }
+
+        result = MyKen::ModelChecker.true_in_model?(a.or(b), model)
+        expect(result).to be true
+      end
+    end
+    context 'complex statement' do
+      it do
+        a = MyKen::Statements::Statement.new("A")
+        b = MyKen::Statements::Statement.new("B")
+        c = MyKen::Statements::Statement.new("C")
+        d = MyKen::Statements::Statement.new("D")
+        model = {
+          "A" => true,
+          "B" => false,
+          "C" => false,
+          "D" => true
+        }
+
+        stmt = a.or(b).and(c.or(d.not))
+        # True and False => False
+        result = MyKen::ModelChecker.true_in_model?(stmt, model)
+        expect(result).to be false
+      end
+    end
+  end
+  describe '.entail?' do
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      b = MyKen::Statements::Statement.new("B")
+
+      prop_kb = MyKen::PropositionalKB.new
+      prop_kb.assert(a.⊃(b))
+      prop_kb.assert(a)
+
+      expect(MyKen::ModelChecker.entail?(prop_kb, b)).to be true
+    end
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      b = MyKen::Statements::Statement.new("B")
+
+      prop_kb = MyKen::PropositionalKB.new
+      prop_kb.assert(a.⊃(b))
+      prop_kb.assert(b.not)
+
+      expect(MyKen::ModelChecker.entail?(prop_kb, a.not)).to be true
+    end
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      b = MyKen::Statements::Statement.new("B")
+      c = MyKen::Statements::Statement.new("C")
+
+      prop_kb = MyKen::PropositionalKB.new
+      prop_kb.assert(a.⊃(b))
+      prop_kb.assert(b.⊃(c))
+      prop_kb.assert(a)
+
+      expect(MyKen::ModelChecker.entail?(prop_kb, c)).to be true
+    end
+    it do
+      a = MyKen::Statements::Statement.new("A")
+      b = MyKen::Statements::Statement.new("B")
+      c = MyKen::Statements::Statement.new("C")
+
+      prop_kb = MyKen::PropositionalKB.new
+      prop_kb.assert(a.⊃(b))
+      prop_kb.assert(b.⊃(c))
+      prop_kb.assert(c.not)
+
+      expect(MyKen::ModelChecker.entail?(prop_kb, a.not)).to be true
+    end
+  end
   context 'simple knowledge base' do
     let(:as1) do
       MyKen::Statements::AtomicStatement.new(true, "as1")
