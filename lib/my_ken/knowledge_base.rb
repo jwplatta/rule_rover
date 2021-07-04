0989cc370134945bf379a0a1b1a1b16b22ba9d94
Joseph Platta <jwplatta@gmail.com>
2021-07-04 12:51:53 -0700

    create separate classes for prop and predicate knowledge bases

--- lib/my_ken/knowledge_base.rb
+++ lib/my_ken/knowledge_base.rb
@@ -1,6 +1,7 @@
 module MyKen
   class KnowledgeBase
     def initialize(&block)
+      @clauses = [] # TODO:
       @statements = []
       @atomic_statements = []
       yield self
@@ -10,6 +11,7 @@ module MyKen
 
     def add_fact(statement)
       raise StandardError.new('Statement must be assigned a truth value.') if statement.value.nil?
+
       @atomic_statements << statement if statement.is_a? Statements::AtomicStatement
       @statements << statement
     end
@@ -34,6 +36,66 @@ module MyKen
       statements.map do |stmt|
         stmt.to_s
       end.join("\n")
+    end
+  end
+
+  class PropositionalKB
+    attr_reader :clauses
+
+    def initialize()
+      @clauses = []
+    end
+
+    def to_statement
+      clauses[1..].reduce(clauses[0]) { |stmt, cls| stmt.and(cls) }
+    end
+
+    def assert(statement)
+      new_clauses = MyKen::Statements.to_conjunctive_normal_form(statement).then do |stmt|
+        MyKen::Statements.conjuncts(stmt)
+      end.reject do |cls|
+        clauses.include? cls
+      end
+
+      @clauses = (clauses << new_clauses).flatten
+    end
+
+    def deny(statement)
+      conjuncts = MyKen::Statements.to_conjunctive_normal_form(statement).then do |stmt|
+        MyKen::Statements.conjuncts(stmt)
+      end
+
+      @clauses = clauses.reject { |clause| conjuncts.include? clause }
+    end
+
+    def query(statement)
+      MyKen::Statements.to_conjunctive_normal_form(statement).then do |stmt|
+        MyKen::Statements.conjuncts(stmt)
+      end.reduce(true) do |result, cls|
+        clauses.include? cls and result
+      end
+    end
+  end
+
+  class PredicateKB
+    attr_reader :clauses, :variables, :predicates
+
+    def initialize
+      @clauses = []
+      @variables = []
+      @predicates = []
+    end
+
+    def to_statement
+    end
+
+    def assert(statement)
+    end
+
+    def deny(statement)
+    end
+
+    def query(statement)
     end
   end
 end
--- spec/knowledge_base_spec.rb
+++ spec/knowledge_base_spec.rb
@@ -51,4 +51,110 @@ describe MyKen::KnowledgeBase do
       expect(kb.value).to be true
     end
   end
+
+  describe 'propositional knowledge base' do
+    context '#assert' do
+      it 'adds an atomic to the KB' do
+        a = MyKen::Statements::Statement.new("A")
+        b = MyKen::Statements::Statement.new("B")
+        prop_kb = MyKen::PropositionalKB.new
+        prop_kb.assert(a)
+        prop_kb.assert(b)
+        expect(prop_kb.clauses.size).to eq 2
+      end
+      context 'adding complex statements' do
+        it do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b))
+          expect(prop_kb.clauses.size).to eq 1
+        end
+        it 'converts to CNF beforing adding to KB' do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.⊃(b))
+          expect(prop_kb.clauses.first.to_s).to eq a.not.or(b).to_s
+        end
+      end
+    end
+    context '#deny' do
+      it 'removes an atomic from KB' do
+        a = MyKen::Statements::Statement.new("A")
+        prop_kb = MyKen::PropositionalKB.new
+        prop_kb.assert(a)
+        expect(prop_kb.clauses.size).to eq 1
+        prop_kb.deny(a)
+        expect(prop_kb.clauses.size).to eq 0
+      end
+      context 'removing complex statements' do
+        it do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b))
+          expect(prop_kb.clauses.size).to eq 1
+          prop_kb.deny(b.or(a))
+          expect(prop_kb.clauses.size).to eq 0
+        end
+        it do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          c = MyKen::Statements::Statement.new("C")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b).and(c))
+          expect(prop_kb.clauses.size).to eq 2
+          prop_kb.deny(c.and(b.or(a)))
+          expect(prop_kb.clauses.size).to eq 0
+        end
+        it do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          c = MyKen::Statements::Statement.new("C")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b).and(c))
+          expect(prop_kb.clauses.size).to eq 2
+          prop_kb.deny(c.and(b.or(a)))
+          expect(prop_kb.clauses.size).to eq 0
+        end
+        it do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          c = MyKen::Statements::Statement.new("C")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b).and(c))
+          expect(prop_kb.clauses.size).to eq 2
+          prop_kb.deny(c.and(b.or(a)))
+          expect(prop_kb.clauses.size).to eq 0
+        end
+        it 'does not add clauses already in the KB' do
+          a = MyKen::Statements::Statement.new("A")
+          b = MyKen::Statements::Statement.new("B")
+          c = MyKen::Statements::Statement.new("C")
+          d = MyKen::Statements::Statement.new("D")
+          prop_kb = MyKen::PropositionalKB.new
+          prop_kb.assert(a.or(b).and(c))
+          expect(prop_kb.clauses.size).to eq 2
+          prop_kb.assert(d.and(b.or(a)))
+          expect(prop_kb.clauses.size).to eq 3
+        end
+      end
+    end
+    context '#query' do
+      it do
+        a = MyKen::Statements::Statement.new("A")
+        b = MyKen::Statements::Statement.new("B")
+        c = MyKen::Statements::Statement.new("C")
+        d = MyKen::Statements::Statement.new("D")
+        prop_kb = MyKen::PropositionalKB.new
+        prop_kb.assert(a.or(b).and(c))
+
+        expect(prop_kb.query(c)).to be true
+        expect(prop_kb.query(a.or(b).and(c))).to be true
+        expect(prop_kb.query(a.or(b))).to be true
+        expect(prop_kb.query(a.or(b).and(d))).to be false
+      end
+    end
+  end
 end
