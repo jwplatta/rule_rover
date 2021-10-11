6661598a225731840f9f0985732886beec826946
Joseph Platta <jwplatta@gmail.com>
2021-10-10 18:08:53 -0700

    update requires

--- lib/my_ken.rb
+++ lib/my_ken.rb
@@ -4,10 +4,13 @@ require_relative "./my_ken/version"
 require_relative "./my_ken/boolean_monkey_patch"
 require_relative "./my_ken/knowledge_base"
 require_relative "./my_ken/statements"
+require_relative "./my_ken/statements/proposition"
+require_relative "./my_ken/propositional_kb"
+require_relative "./my_ken/statements/to_cnf"
 require_relative "./my_ken/model_checker"
 require_relative "./my_ken/conjunctive_normal_form"
 require_relative "./my_ken/resolver"
-require_relative "./my_ken/statement_parser.rb"
+require_relative "./my_ken/forward_chaining.rb"
 
 module MyKen
   class Error < StandardError; end
