ad89f19867ecac660d3571cf894788c6a44bd81f
Joseph Platta <jwplatta@gmail.com>
2024-03-20 09:14:42 +0100

    eliminate biconditional spec

--- spec/propositional_logic/sentences/biconditional_spec.rb
+++ spec/propositional_logic/sentences/biconditional_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe RuleRover::PropositionalLogic::Sentences::Biconditional do
+  Factory = RuleRover::PropositionalLogic::Sentences::Factory
+
   it 'does not raise' do
     expect { described_class.new(nil, nil) }.not_to raise_error
+  end
+
+  describe '#eliminate_biconditionals' do
+    it 'returns a conjunction of conditionals' do
+      sentence = Factory.build("a", :iff, "b")
+      expects = Factory.build(["a", :then, "b"], :and, ["b", :then, "a"])
+      expect(sentence.eliminate_biconditionals).to eq(expects)
+    end
   end
 end
