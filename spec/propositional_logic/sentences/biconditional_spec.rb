56e67047c99946e9fc3b0c09407c5b87833fe13a
Joseph Platta <jwplatta@gmail.com>
2024-03-20 09:25:54 +0100

    wrap factory class in method in specs

--- spec/propositional_logic/sentences/biconditional_spec.rb
+++ spec/propositional_logic/sentences/biconditional_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe RuleRover::PropositionalLogic::Sentences::Biconditional do
-  Factory = RuleRover::PropositionalLogic::Sentences::Factory
-
   it 'does not raise' do
     expect { described_class.new(nil, nil) }.not_to raise_error
   end
 
   describe '#eliminate_biconditionals' do
     it 'returns a conjunction of conditionals' do
-      sentence = Factory.build("a", :iff, "b")
-      expects = Factory.build(["a", :then, "b"], :and, ["b", :then, "a"])
+      sentence = sentence_factory.build("a", :iff, "b")
+      expects = sentence_factory.build(["a", :then, "b"], :and, ["b", :then, "a"])
       expect(sentence.eliminate_biconditionals).to eq(expects)
     end
+  end
+
+  def sentence_factory
+    RuleRover::PropositionalLogic::Sentences::Factory
   end
 end
--- spec/propositional_logic/sentences/conditional_spec.rb
+++ spec/propositional_logic/sentences/conditional_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe RuleRover::PropositionalLogic::Sentences::Conditional do
-  Factory = RuleRover::PropositionalLogic::Sentences::Factory
   it 'does not raise' do
     expect { described_class.new(nil, nil) }.not_to raise_error
   end
 
   describe '#eliminate_conditionals' do
     it 'returns a disjunction with antecedent negated' do
-      sentence = Factory.build("a", :then, "b")
-      expects = Factory.build(:not, "a", :or, "b")
+      sentence = sentence_factory.build("a", :then, "b")
+      expects = sentence_factory.build(:not, "a", :or, "b")
       expect(sentence.eliminate_conditionals).to eq(expects)
     end
+  end
+
+  def sentence_factory
+    RuleRover::PropositionalLogic::Sentences::Factory
   end
 end
