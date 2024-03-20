f07c9662e1f2ec89489591e3b768ded002613c89
Joseph Platta <jwplatta@gmail.com>
2024-03-20 09:16:10 +0100

    material implication spec

--- spec/propositional_logic/sentences/conditional_spec.rb
+++ spec/propositional_logic/sentences/conditional_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe RuleRover::PropositionalLogic::Sentences::Conditional do
+  Factory = RuleRover::PropositionalLogic::Sentences::Factory
   it 'does not raise' do
     expect { described_class.new(nil, nil) }.not_to raise_error
   end
 
   describe '#eliminate_conditionals' do
+    it 'returns a disjunction with antecedent negated' do
+      sentence = Factory.build("a", :then, "b")
+      expects = Factory.build(:not, "a", :or, "b")
+      expect(sentence.eliminate_conditionals).to eq(expects)
+    end
   end
 end
