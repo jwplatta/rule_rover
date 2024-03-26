d774ac6d8d72372a455eb4b452650bee25a6fe13
Joseph Platta <jwplatta@gmail.com>
2024-03-26 11:40:54 +0100

    feat: add inference engine types to constants

--- lib/rule_rover/constants.rb
+++ lib/rule_rover/constants.rb
@@ -1,4 +1,5 @@
 module RuleRover
   CONNECTIVES=[:and, :or, :then, :iff]
   OPERATORS=[:not, :and, :or, :then, :iff]
+  ENGINES=[:model_checking, :resolution, :forward_chaining, :backward_chaining]
 end
