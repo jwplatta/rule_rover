037f119d14a4d308c12689b2d6e2f4e8a5707460
Joseph Platta <jwplatta@gmail.com>
2024-03-29 12:32:27 +0100

    feat: constant_symbol class method to check for valid constants. a valid constant is a string that starts with a capital letter

--- spec/first_order_logic/sentences/constant_symbol_spec.rb
+++ spec/first_order_logic/sentences/constant_symbol_spec.rb
@@ -4,4 +4,13 @@ describe RuleRover::FirstOrderLogic::Sentences::ConstantSymbol do
   it 'does not raise' do
     expect { described_class.new('Aristotle') }.not_to raise_error
   end
+
+  describe '.valid?' do
+    it 'returns true for a string starting with a capital letter' do
+      expect(described_class.valid?('Aristotle')).to be(true)
+    end
+    it 'returns false for a string starting with a lowercase letter' do
+      expect(described_class.valid?('aristotle')).to be(false)
+    end
+  end
 end
