9c84a1cf76c980aa6cf6c6fde2e085ed5e1a9370
Joseph Platta <jwplatta@gmail.com>
2024-03-29 12:33:00 +0100

    feat: class method on function symbol to check for valid function names. a valid function name is a symbol that starts with @

--- lib/rule_rover/first_order_logic/sentences/function_symbol.rb
+++ lib/rule_rover/first_order_logic/sentences/function_symbol.rb
@@ -2,6 +2,7 @@ module RuleRover::FirstOrderLogic::Sentences
   class FunctionSymbol
     class << self
       def valid?(name)
+        name.is_a? Symbol and /^@/.match?(name.to_s)
       end
     end
 
--- spec/first_order_logic/sentences/function_symbol_spec.rb
+++ spec/first_order_logic/sentences/function_symbol_spec.rb
@@ -4,4 +4,16 @@ describe RuleRover::FirstOrderLogic::Sentences::FunctionSymbol do
   it 'does not raise' do
     expect { described_class.new }.not_to raise_error
   end
+
+  describe '.valid?' do
+    it 'returns true for a symbol starting with an @' do
+      expect(described_class.valid?(:@teacher_of)).to be(true)
+    end
+    it 'returns false for a symbol starting with a letter' do
+      expect(described_class.valid?(:teacher_of)).to be(false)
+    end
+    it 'returns false for a string' do
+      expect(described_class.valid?("@teacher_of")).to be(false)
+    end
+  end
 end
