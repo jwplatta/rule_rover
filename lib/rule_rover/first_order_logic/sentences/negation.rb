677496964a850194994875fd40fab7776ca96890
Joseph Platta <jwplatta@gmail.com>
2024-04-02 15:17:53 +0200

    feat: add negation to FOL

---
+++ lib/rule_rover/first_order_logic/sentences/negation.rb
@@ -0,0 +1,77 @@
+module RuleRover::FirstOrderLogic::Sentences
+  class Negation
+    def initialize(sentence_or_term)
+      @sentence = sentence_or_term
+    end
+
+    attr_reader :sentence
+
+    def evaluate(model)
+      not sentence.evaluate(model)
+    end
+
+    def to_s
+      "[:not #{sentence}]"
+    end
+
+    # def symbol
+    #   if is_atomic?
+    #     sentence.symbol
+    #   else
+    #     raise SentenceNotInCNF.new
+    #   end
+    # end
+
+    # def symbols
+    #   sentence.symbols
+    # end
+
+    # def atoms
+    #   if is_atomic?
+    #     Set.new([self])
+    #   else
+    #     sentence.atoms
+    #   end
+    # end
+
+    # def eliminate_biconditionals
+    #   Negation.new(sentence.eliminate_biconditionals)
+    # end
+
+    # def eliminate_conditionals
+    #   Negation.new(sentence.eliminate_conditionals)
+    # end
+
+    # def distribute
+    #   self
+    # end
+
+    # def elim_double_negations
+    #   if sentence.is_a? self.class and sentence.sentence.is_atomic?
+    #     sentence.sentence
+    #   else
+    #     Negation.new(sentence.elim_double_negations)
+    #   end
+    # end
+
+    # def de_morgans_laws
+    #   if sentence.is_a? Conjunction
+    #     Disjunction.new(
+    #       Negation.new(sentence.left),
+    #       Negation.new(sentence.right)
+    #     )
+    #   elsif sentence.is_a? Disjunction
+    #     Conjunction.new(
+    #       Negation.new(sentence.left),
+    #       Negation.new(sentence.right)
+    #     )
+    #   else
+    #     Negation.new(sentence.de_morgans_laws)
+    #   end
+    # end
+
+    # def is_atomic?
+    #   sentence.is_atomic?
+    # end
+  end
+end
