09a7c8edbb0ec3de62f961fd6cc66948ebc6b896
Joseph Platta <jwplatta@gmail.com>
2024-04-05 23:53:17 -0500

    feat: create knowledge base for FOL

--- lib/rule_rover/first_order_logic/knowledge_base.rb
+++ lib/rule_rover/first_order_logic/knowledge_base.rb
@@ -1,8 +1,27 @@
 require 'set'
 
 module RuleRover::FirstOrderLogic
+  class QueryNotSinglePropositionSymbol < StandardError; end
+  class KnowledgeBaseNotDefinite < StandardError; end
+  class InvalidEngine < StandardError; end
+
   class KnowledgeBase
-    def initialize(engine: :forward_chaining)
+    def initialize(engine: :forward_chaining, sentences: [])
+      @constants = []
+      @functions = []
+      @predicates = []
+      @sentences = sentences
+      @engine = engine
+    end
+
+    attr_reader :constants, :functions, :predicates, :sentences, :engine
+
+    def connectives
+      @connectives ||= RuleRover::FirstOrderLogic::CONNECTIVES
+    end
+
+    def operators
+      @operators ||= RuleRover::FirstOrderLogic::OPERATORS
     end
   end
 end
