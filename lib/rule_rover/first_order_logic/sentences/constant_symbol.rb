57ceff686def4ad564dd28a74ec0f5f35b29e92a
Joseph Platta <jwplatta@gmail.com>
2024-04-13 09:20:55 -0500

    WIP: add note for SymbolBase class

--- lib/rule_rover/first_order_logic/sentences/constant_symbol.rb
+++ lib/rule_rover/first_order_logic/sentences/constant_symbol.rb
@@ -10,7 +10,22 @@ module RuleRover::FirstOrderLogic::Sentences
     def initialize(name)
       @name = name
     end
+
     attr_reader :name
+
+    # NOTE: consider creating a SymbolBase class that
+    # implements the #==, #eql?, #hash, and #to_s methods
+    def ==(other)
+      to_s == other.to_s
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      to_s.hash
+    end
 
     def to_s
       name
