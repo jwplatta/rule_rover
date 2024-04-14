615789e71ffc2167cfca9ccbbd782e1f5829e48c
Joseph Platta <jwplatta@gmail.com>
2024-04-14 15:54:05 -0500

    WIP: continue building out the other conditions of the unification algorithm

--- lib/rule_rover/first_order_logic/algorithms/unification.rb
+++ lib/rule_rover/first_order_logic/algorithms/unification.rb
@@ -4,18 +4,31 @@ module RuleRover::FirstOrderLogic::Algorithms
       def run(expression_x, expression_y, substitution={})
         if not substitution
           false
-        elsif same_variable?(expression_x, expression_y)
+        elsif is_variable?(expression_x) and expression_x == expression_y
           substitution
+        elsif is_variable?(expression_x)
+          unify_variable(expression_x, expression_y, substitution)
+        elsif is_variable?(expression_y)
+          unify_variable(expression_y, expression_x, substitution)
         else
           false
         end
       end
 
       def unify_variable(variable, expression, substitution)
+        if substitution.key? variable
+          run(substitution[variable], expression, substitution)
+        elsif substitution.key? expression
+          run(variable, substitution[expression], substitution)
+        # TODO: check occur variable, expression
+        else
+          substitution[variable] = expression
+          substitution
+        end
       end
 
-      def same_variable?(expression_x, expression_y)
-        /^[a-z]/.match?(expression_x) and /^[a-z]/.match?(expression_y) and expression_x == expression_y
+      def is_variable?(expression)
+        expression.is_a? RuleRover::FirstOrderLogic::Sentences::Variable
       end
     end
   end
--- spec/first_order_logic/algorithms/unification_spec.rb
+++ spec/first_order_logic/algorithms/unification_spec.rb
@@ -5,20 +5,69 @@ describe RuleRover::FirstOrderLogic::Algorithms::Unification do
     expect { described_class.run(nil, nil, {}) }.not_to raise_error
   end
 
-  describe '.run' do
-    context 'when subsitution is false' do
+  fdescribe '.run' do
+    context 'when substitution is false' do
       it 'returns false' do
-        expect(described_class.run('x', 'y', false)).to be(false)
+        expect(
+          described_class.run(
+            sentence_factory.build('x'),
+            sentence_factory.build('y'),
+            false
+          )
+        ).to be(false)
       end
     end
     context 'when given two variables' do
       context 'when the variables are the same' do
-        fit 'returns a substitution' do
-          expect(described_class.run('x', 'x', {})).to eq({})
+        it 'returns a substitution' do
+          expect(
+            described_class.run(
+              sentence_factory.build('x'),
+              sentence_factory.build('x'),
+              {}
+            )
+          ).to eq({})
         end
       end
-      context 'when the variables are different' do
+      fcontext 'when the variables are different' do
+        it 'returns a substitution' do
+          expect(
+            described_class.run(
+              sentence_factory.build('x'),
+              "Joe",
+              {}
+            )
+          ).to eq({ "x" => "Joe" })
+
+          expect(
+            described_class.run(
+              "Joe",
+              sentence_factory.build("x"),
+              {}
+            )
+          ).to eq({ "x" => "Joe" })
+        end
       end
     end
+    context 'when given a variable and a constant' do
+    end
+  end
+
+  describe '.is_variable?' do
+    context 'when the expression is a variable' do
+      it 'returns true' do
+        expect(
+          described_class.is_variable?(
+            sentence_factory.build('x')
+          )
+        ).to be true
+      end
+    end
+    context 'when the expression is not a variable' do
+    end
+  end
+
+  def sentence_factory
+    RuleRover::FirstOrderLogic::Sentences::Factory
   end
 end
