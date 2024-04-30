4bfb781c71caf61d71d7230f1a076c58e71698f7
Joseph Platta <jwplatta@gmail.com>
2024-04-30 23:43:20 +0200

    feat: add a substitution class

---
+++ lib/rule_rover/first_order_logic/substitution.rb
@@ -0,0 +1,15 @@
+module RuleRover::FirstOrderLogic
+  module Substitution
+    def initialize
+      @mapping = {}
+    end
+
+    attr_reader :mapping
+
+    def add
+    end
+
+    def apply(sentence)
+    end
+  end
+end
---
+++ spec/first_order_logic/substitution_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe RuleRover::FirstOrderLogic::Substitution do
+  def sentence_factory
+    RuleRover::FirstOrderLogic::Sentences::Factory
+  end
+end
