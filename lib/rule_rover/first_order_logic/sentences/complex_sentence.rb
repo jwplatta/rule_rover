5c56f5b4c4a73f34d31faab3b03b2c146c115d05
Joseph Platta <jwplatta@gmail.com>
2024-05-07 23:16:46 +0200

    chore: add a docstring to complex sentence interface class

--- lib/rule_rover/first_order_logic/sentences/complex_sentence.rb
+++ lib/rule_rover/first_order_logic/sentences/complex_sentence.rb
@@ -1,13 +1,21 @@
 module RuleRover::FirstOrderLogic::Sentences
+  # The ComplexSentence class defines an interface for representing expressions that consist
+  # of more than one term. Instances of this class hold references to these sub-expressions,
+  # typically denoted as the left and right parts of the complex sentence.
+  #
+  # == Attributes
+  #
+  # [left]  Represents the left sub-expression of the complex sentence.
+  # [right] Represents the right sub-expression of the complex sentence.
+  #
   class ComplexSentence
+    attr_reader :left, :right
     include Expression
 
     def initialize(left, right)
       @left = left
       @right = right
     end
-
-    attr_reader :left, :right
 
     def constants
       left.constants.merge(right.constants)
