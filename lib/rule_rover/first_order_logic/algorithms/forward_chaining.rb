43910e2950720293715c85a8b55e50d53efa941a
Joseph Platta <jwplatta@gmail.com>
2024-05-09 23:26:39 +0200

    WIP: convert FOL forward chaining to class and add helper methods for enumerating all possible substitutions and identify definite clauses

--- lib/rule_rover/first_order_logic/algorithms/forward_chaining.rb
+++ lib/rule_rover/first_order_logic/algorithms/forward_chaining.rb
@@ -1,6 +1,10 @@
+require_relative '../sentences/unification'
+
 module RuleRover::FirstOrderLogic
   module Algorithms
-    module ForwardChaining
+    class ForwardChaining
+      include RuleRover::FirstOrderLogic::Sentences::Unification
+
       """
       - Requirements:
       - only definite clauses
@@ -12,19 +16,42 @@ module RuleRover::FirstOrderLogic
       - 1. try to unify the query with an existing clause in the database
       - 2. if not, then loop through all the clauses in the database
       """
-      def forward_chain(kb, *query)
-        binding.pry
+      TERM_CLASSES = [
+        RuleRover::FirstOrderLogic::Sentences::PredicateSymbol,
+        RuleRover::FirstOrderLogic::Sentences::FunctionSymbol,
+        RuleRover::FirstOrderLogic::Sentences::ConstantSymbol,
+        RuleRover::FirstOrderLogic::Sentences::Variable,
+      ]
+
+      class << self
+        def forward_chain(kb, query)
+          self.new(kb, query).forward_chain(kb, query)
+        end
       end
 
-      def self.definite_clause?(sentence)
+      def initialize(kb, query)
+        @kb = kb
+        @query = query
+      end
+
+      attr_reader :kb, :query
+
+      def forward_chain(kb, query)
+        kb.sentences.each do |sentence|
+          return true if unify(sentence, query)
+        end
+        false
+      end
+
+      def substitutions(constants, variables)
+        variables.product(constants).map do |variable, constant|
+          { variable => constant }
+        end
+      end
+
+      def definite_clause?(sentence)
         return false unless sentence.is_a? RuleRover::FirstOrderLogic::Sentences::Disjunction
 
-        term_classes = [
-          RuleRover::FirstOrderLogic::Sentences::PredicateSymbol,
-          RuleRover::FirstOrderLogic::Sentences::FunctionSymbol,
-          RuleRover::FirstOrderLogic::Sentences::ConstantSymbol,
-          RuleRover::FirstOrderLogic::Sentences::Variable,
-        ]
         frontier = [sentence.left, sentence.right]
         count = 0
 
@@ -34,8 +61,8 @@ module RuleRover::FirstOrderLogic
           if current.is_a? RuleRover::FirstOrderLogic::Sentences::Disjunction
             frontier.push(current.left, current.right)
           elsif current.is_a? RuleRover::FirstOrderLogic::Sentences::Negation
-            puts 'negation'
-          elsif term_classes.include? current.class
+            next
+          elsif TERM_CLASSES.include? current.class
             count += 1
           else
             return false
--- spec/first_order_logic/algorithms/forward_chaining_spec.rb
+++ spec/first_order_logic/algorithms/forward_chaining_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
 describe RuleRover::FirstOrderLogic::Algorithms::ForwardChaining do
-  xit do
-    kb = RuleRover::FirstOrderLogic::KnowledgeBase.new
-    kb.assert()
-    binding.pry
-    # subject.forward_chain
+  describe '.forward_chain' do
+    context 'knowledge base contains the query' do
+      it do
+        kb = RuleRover::FirstOrderLogic::KnowledgeBase.new
+        kb.assert([:not, ['Alexander', :taught, 'Aristotle']], :or, ['Socrates', :taught, 'Plato'])
+        kb.assert('Alexander', :taught, 'Aristotle')
+        query = sentence_factory.build('Alexander', :taught, 'Aristotle')
+
+        expect(described_class.forward_chain(kb, query)).to be(true)
+      end
+    end
   end
   describe '.definite_clause?' do
     context 'is a clause with a single positive literal' do
-      fit do
+      it do
         sentence = sentence_factory.build([:not, ['Plato', :taught, 'Socrates'], :or, :not, ['Alexander', :taught, 'Aristotle']], :or, ['Aristotle', :taught, 'Alexander'])
-        expect(subject.definite_clause?(sentence)).to be(true)
+        expect(described_class.new(nil, nil).definite_clause?(sentence)).to be(true)
       end
     end
     context 'is a clause with a multiple positive literals' do
       it do
         sentence = sentence_factory.build(:not, ['Plato', :taught, 'Socrates'], :or, :not, ['Alexander', :taught, 'Aristotle'])
+        expect(described_class.new(nil, nil).definite_clause?(sentence)).to be(false)
       end
     end
   end
