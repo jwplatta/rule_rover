813f20fb5b777b05d8b2530e57fc5a76dc0274d7
Joseph Platta <jwplatta@gmail.com>
2024-05-24 23:05:36 -0400

    fix: add correct implemented entailment engine names to constants

--- lib/rule_rover/first_order_logic/constants.rb
+++ lib/rule_rover/first_order_logic/constants.rb
@@ -1,6 +1,6 @@
 module RuleRover::FirstOrderLogic
   CONNECTIVES=%i[and or then iff].freeze
   OPERATORS=%i[all some equals not and or then iff].freeze
-  ENGINES=%i[model_checking resolution forward_chaining backward_chaining].freeze
+  ENGINES=%i[matching forward_chaining backward_chaining].freeze
   QUANTIFIERS=%i[all some].freeze
 end
