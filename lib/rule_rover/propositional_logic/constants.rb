7f99e740c91d1ba1bdd428969c6e35daf647a7a8
Joseph Platta <jwplatta@gmail.com>
2024-05-27 19:15:45 +0200

    chore: use nested modules

--- lib/rule_rover/propositional_logic/constants.rb
+++ lib/rule_rover/propositional_logic/constants.rb
@@ -1,5 +1,7 @@
-module RuleRover::PropositionalLogic
-  CONNECTIVES=%i[and or then iff].freeze
-  OPERATORS=%i[not and or then iff].freeze
-  ENGINES=%i[model_checking resolution forward_chaining backward_chaining].freeze
+module RuleRover
+  module PropositionalLogic
+    CONNECTIVES=%i[and or then iff].freeze
+    OPERATORS=%i[not and or then iff].freeze
+    ENGINES=%i[model_checking resolution forward_chaining backward_chaining].freeze
+  end
 end
