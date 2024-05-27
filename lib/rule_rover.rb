da4f96f29368f5fb4a0087f1dda12347ef6036d6
Joseph Platta <jwplatta@gmail.com>
2024-05-27 19:08:49 +0200

    chore: shorten requires and set default engine to forward chaining

--- lib/rule_rover.rb
+++ lib/rule_rover.rb
@@ -2,47 +2,32 @@
 
 require_relative "./rule_rover/version"
 require_relative "./rule_rover/sentence_not_well_formed_error.rb"
+require_relative "./rule_rover/sentence_not_in_cnf.rb"
 require_relative "./rule_rover/propositional_logic/constants.rb"
-require_relative "./rule_rover/propositional_logic/knowledge_base.rb"
-require_relative "./rule_rover/propositional_logic/sentences/sentence.rb"
-require_relative "./rule_rover/propositional_logic/sentences/conjunction.rb"
-require_relative "./rule_rover/propositional_logic/sentences/disjunction.rb"
-require_relative "./rule_rover/propositional_logic/sentences/negation.rb"
-require_relative "./rule_rover/propositional_logic/sentences/conditional.rb"
-require_relative "./rule_rover/propositional_logic/sentences/biconditional.rb"
-require_relative "./rule_rover/propositional_logic/sentences/atomic.rb"
-require_relative "./rule_rover/propositional_logic/sentences/factory.rb"
 require_relative "./rule_rover/propositional_logic/algorithms/logic_algorithm_base.rb"
-require_relative "./rule_rover/propositional_logic/algorithms/model_checking.rb"
-require_relative "./rule_rover/propositional_logic/algorithms/resolution.rb"
-require_relative "./rule_rover/propositional_logic/algorithms/forward_chaining.rb"
-require_relative "./rule_rover/propositional_logic/algorithms/backward_chaining.rb"
+require_relative "./rule_rover/propositional_logic/sentences/sentence.rb"
 require_relative "./rule_rover/first_order_logic/constants.rb"
-require_relative "./rule_rover/first_order_logic/errors.rb"
+require_relative "./rule_rover/first_order_logic/sentences/substitution.rb"
+require_relative "./rule_rover/first_order_logic/sentences/standardize_apart.rb"
 require_relative "./rule_rover/first_order_logic/sentences/expression.rb"
+require_relative "./rule_rover/first_order_logic/sentences/predicate_symbol.rb"
+require_relative "./rule_rover/first_order_logic/sentences/function_symbol.rb"
+require_relative "./rule_rover/first_order_logic/sentences/constant_symbol.rb"
+require_relative "./rule_rover/first_order_logic/sentences/variable.rb"
 require_relative "./rule_rover/first_order_logic/sentences/complex_sentence.rb"
 require_relative "./rule_rover/first_order_logic/sentences/quantifier.rb"
-require_relative "./rule_rover/first_order_logic/sentences/factory.rb"
-require_relative "./rule_rover/first_order_logic/sentences/constant_symbol.rb"
-require_relative "./rule_rover/first_order_logic/sentences/function_symbol.rb"
-require_relative "./rule_rover/first_order_logic/sentences/predicate_symbol.rb"
-require_relative "./rule_rover/first_order_logic/sentences/variable.rb"
-require_relative "./rule_rover/first_order_logic/sentences/conjunction.rb"
-require_relative "./rule_rover/first_order_logic/sentences/disjunction.rb"
-require_relative "./rule_rover/first_order_logic/sentences/biconditional.rb"
-require_relative "./rule_rover/first_order_logic/sentences/conditional.rb"
-require_relative "./rule_rover/first_order_logic/sentences/negation.rb"
-require_relative "./rule_rover/first_order_logic/sentences/universal_quantifier.rb"
-require_relative "./rule_rover/first_order_logic/sentences/existential_quantifier.rb"
-require_relative "./rule_rover/first_order_logic/sentences/equals.rb"
-require_relative "./rule_rover/first_order_logic/sentences/standardize_apart.rb"
-require_relative "./rule_rover/first_order_logic/sentences/unification.rb"
-require_relative "./rule_rover/first_order_logic/algorithms/forward_chaining.rb"
-require_relative "./rule_rover/first_order_logic/algorithms/backward_chaining.rb"
-require_relative "./rule_rover/first_order_logic/knowledge_base.rb"
+
+
+Dir.glob(File.join(__dir__, 'rule_rover/propositional_logic/**/*.rb')).sort.each do |file|
+  require_relative file
+end
+
+Dir.glob(File.join(__dir__, 'rule_rover/first_order_logic/**/*.rb')).sort.each do |file|
+  require_relative file
+end
 
 module RuleRover
-  def knowledge_base(system: :first_order, engine: :model_checking, &block)
+  def knowledge_base(system: :first_order, engine: :forward_chaining, &block)
     kb = if system == :first_order
       RuleRover::FirstOrderLogic::KnowledgeBase.new(engine: engine)
     elsif system == :propositional
