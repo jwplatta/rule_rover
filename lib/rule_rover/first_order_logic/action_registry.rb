efd13332b034e09731a9042ebd3a59d676badb3b
Joseph Platta <jwplatta@gmail.com>
2024-06-13 13:02:45 +0200

    WIP: edits to action registry. will use this class to manage all logic associated with defining and calling actions.

--- lib/rule_rover/first_order_logic/action_registry.rb
+++ lib/rule_rover/first_order_logic/action_registry.rb
@@ -1,25 +1,17 @@
 module RuleRover::FirstOrderLogic
   class ActionRegistry
-    def initialize(kb: nil, actions: {})
+    def initialize(kb: nil, actions: {}, rule_action_map: {})
       @kb = kb
       @actions = actions
+      @rule_action_map = rule_action_map
     end
 
-    attr_reader :kb, :actions
-
-    def call(name, **params)
-      # find(name).call(**params)
-      binding.pry
-    end
-
-    def find(name)
-      actions.fetch(name, {})
-    end
+    attr_reader :kb, :actions, :rule_action_map
 
     def add(name, *param_names, &block)
       raise ArgumentError.new("Action already exists: #{name}") if @actions[name]
 
-      @actions[name] = Action.new(
+      @actions[name] = new_action(
         name: name,
         param_names: param_names,
         func: block
@@ -28,13 +20,46 @@ module RuleRover::FirstOrderLogic
       @actions[name]
     end
 
-    # Adds a new action to the registry, overwriting any existing action with the same name.
-    def add!
-      @actions[name] = new_action
-    end
-
     def remove(name)
       @actions.delete(name)
+    end
+
+    def map_rule_to_action(rule, action_name, **params)
+      raise ArgumentError.new("Action does not exist: #{action_name}") unless exists?(action_name)
+      unless rule.is_a? RuleRover::FirstOrderLogic::Sentences::Conditional
+        raise ArgumentError.new("Rule must be a conditional")
+      end
+
+      rule_action_map[rule] ||= []
+      rule_action_map[rule] << { name: action_name, params: params }
+    end
+
+    def call_rule_actions(rule)
+      # NOTE: will need to use the mapping stored on the rule from standardize_apart
+      # in order to map the rule's variables to the action's parameters
+      actions = rule_action_map.fetch(rule, [])
+      actions.each do |action|
+        call(action.name, **action.params)
+      end
+    end
+
+    def call(name, **params)
+      find(name).call(**params)
+      binding.pry
+    end
+
+    def exists?(name)
+      find(name) ? true : false
+    end
+
+    def find(name)
+      actions.fetch(name, nil)
+    end
+
+    private
+
+    def new_action(name, *param_names, &block)
+      Action.new(name: name, param_names: param_names, func: block)
     end
   end
 end
--- spec/first_order_logic/action_registry_spec.rb
+++ spec/first_order_logic/action_registry_spec.rb
@@ -1,7 +1,43 @@
 require "spec_helper"
 
 describe RuleRover::FirstOrderLogic::ActionRegistry do
-  it "does not raise" do
+  fit "does not raise" do
     expect { described_class.new }.not_to raise_error
+  end
+
+  describe "#add" do
+    context 'when adding a net action' do
+      it 'returns the new action' do
+      end
+    end
+    context 'when adding a net action that already exists' do
+      it 'raises an error' do
+      end
+    end
+  end
+
+  describe '#map_rule_to_action' do
+    context 'when the rule is not a conditional' do
+      it 'raises an error' do
+      end
+    end
+    context 'when the action does not exist' do
+      it 'raises an error' do
+      end
+    end
+    context 'when the rule is a conditional and action exists' do
+      it 'maps the rule to the action' do
+      end
+    end
+  end
+
+  describe '#call' do
+  end
+
+  describe '#call_rule_actions' do
+    context 'when rule has single action' do
+    end
+    context 'when rule has multiple actions' do
+    end
   end
 end
