51aa594efc59c3b1c339aea901d7492735139bed
Joseph Platta <jwplatta@gmail.com>
2024-06-16 14:52:25 +0200

    feat: implement an action class

--- lib/rule_rover/first_order_logic/action.rb
+++ lib/rule_rover/first_order_logic/action.rb
@@ -1,13 +1,11 @@
 module RuleRover::FirstOrderLogic
+  class ActionRequiresKeywordParameters < ArgumentError; end
+  class ActionMissingParameter < ArgumentError; end
 
   class Action
     def initialize(func, name: nil, param_names: [])
-      unless func_parameters = func.parameters.map { |_, param_name| param_name }.sort == param_names.sort
-        raise ArgumentError, "Invalid function parameters: #{func_parameters}"
-      end
-
       unless func.parameters.all? { |param_type, _| param_type == :keyreq}
-        raise ArgumentError, "Must use keyword parameters in Action function."
+        raise ActionRequiresKeywordParameters.new
       end
 
       @func = func
@@ -18,10 +16,8 @@ module RuleRover::FirstOrderLogic
     attr_reader :name, :param_names, :func
 
     def call(**params)
-      raise ArgumentError, "Invalid number of parameters" unless param_names.length == params.length
-
       param_names.each do |param_name|
-        raise ArgumentError, "Missing parameter: #{param_name}" unless params.key?(param_name)
+        raise ActionMissingParameter.new(param_name) unless params.key?(param_name)
       end
 
       @func.call(**params)
--- spec/first_order_logic/action_spec.rb
+++ spec/first_order_logic/action_spec.rb
@@ -2,6 +2,48 @@ require "spec_helper"
 
 describe RuleRover::FirstOrderLogic::Action do
   it "does not raise" do
-    expect { described_class.new }.not_to raise_error
+    expect { described_class.new(Proc.new {}) }.not_to raise_error
+  end
+  describe "#initialize" do
+    context 'when the function parameters are not keyword parameters' do
+      it 'raises an error' do
+        expect { described_class.new(
+          Proc.new { |param1| puts param1 },
+          name: :action1,
+          param_names: [:param1]
+        ) }.to raise_error(ArgumentError)
+      end
+    end
+    context 'when the function parameters match the param_names' do
+      it 'does not raise' do
+        new_action = described_class.new(
+          Proc.new { |param1:| puts param1 },
+          name: :action1,
+          param_names: [:param1]
+        )
+        expect(new_action).to be_a(RuleRover::FirstOrderLogic::Action)
+        expect(new_action.name).to eq(:action1)
+        expect(new_action.param_names).to eq([:param1])
+      end
+    end
+  end
+  describe '#call' do
+    let(:action) do
+      described_class.new(
+        Proc.new { |param1:| param1 },
+        name: :action1,
+        param_names: [:param1]
+      )
+    end
+    context 'when the parameters are missing' do
+      it 'raises an error' do
+        expect{ action.call(**{}) }.to raise_error(ArgumentError)
+      end
+    end
+    context 'when the parameters are present' do
+      it 'calls the function' do
+        expect(action.call(param1: "value")).to eq("value")
+      end
+    end
   end
 end
