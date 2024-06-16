13d67d80245baa896dae6de16c77dcde0bcb99a0
Joseph Platta <jwplatta@gmail.com>
2024-06-16 14:15:21 +0200

    feat: store substitution as an instance var

--- lib/rule_rover/first_order_logic/sentences/substitution.rb
+++ lib/rule_rover/first_order_logic/sentences/substitution.rb
@@ -1,33 +1,64 @@
 module RuleRover::FirstOrderLogic::Sentences
   module Substitution
-    def substitute(mapping = {})
-      # TODO: need to check that mapping uses Variable objects as keys
-      return self unless mapping
+    def substitution
+      @substitution ||= {}
+    end
+
+    def substitution=(subst)
+      @substitution = subst
+    end
+
+    def substitute(subst = {})
+      return self unless subst
+
+      subst.each do |key, _|
+        raise ArgumentError, "Substitution key must be a Variable" unless key.is_a? Variable
+      end
 
       if is_a? Variable
-        mapping[self] || self
+        subst[self] || self
       elsif is_a? ConstantSymbol
         self
       elsif is_a? PredicateSymbol
         PredicateSymbol.new(
           name: name,
-          subjects: subjects.map { |term| term.substitute(mapping) },
-          objects: objects.map { |term| term.substitute(mapping) }
-        )
+          subjects: subjects.map { |term| term.substitute(subst) },
+          objects: objects.map { |term| term.substitute(subst) },
+        ).tap do |new_sent|
+          new_sent.substitution = subst
+          new_sent.standardization = standardization if standardization
+        end
       elsif is_a? FunctionSymbol
         FunctionSymbol.new(
           name: name,
-          args: args.map { |term| term.substitute(mapping) }
-        )
+          args: args.map { |term| term.substitute(subst) }
+        ).tap do |new_sent|
+          new_sent.substitution = subst
+          new_sent.standardization = standardization if standardization
+        end
       elsif [Conjunction, Disjunction, Conditional, Biconditional, Equals].include? self.class
-        self.class.new(left.substitute(mapping), right.substitute(mapping))
+        self.class.new(
+          left.substitute(subst),
+          right.substitute(subst)
+        ).tap do |new_sent|
+          new_sent.substitution = subst
+          new_sent.standardization = standardization if standardization
+        end
       elsif is_a? Negation
-        Negation.new(sentence.substitute(mapping))
+        Negation.new(sentence.substitute(subst)).tap do |new_negation|
+          new_negation.substitution = subst
+        end.tap do |new_sent|
+          new_sent.substitution = subst
+          new_sent.standardization = standardization if standardization
+        end
       elsif [ExistentialQuantifier, UniversalQuantifier].include? self.class
         self.class.new(
-          vars.map { |var| var.substitute(mapping) },
-          sentence.substitute(mapping)
-        )
+          vars.map { |var| var.substitute(subst) },
+          sentence.substitute(subst)
+        ).tap do |new_sent|
+          new_sent.substitution = subst
+          new_sent.standardization = standardization if standardization
+        end
       else
         raise NotImplementedError, "Substitution not implemented for #{self.class}"
       end
