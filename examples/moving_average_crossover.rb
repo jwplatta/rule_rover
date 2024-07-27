562137d4dcb77c75502c64f3542d68776b51a980
Joseph Platta <jwplatta@gmail.com>
2024-07-27 13:11:44 -0500

    feat: complete a very simple threshold trading strategy

--- examples/moving_average_crossover.rb
+++ examples/moving_average_crossover.rb
@@ -47,21 +47,21 @@ end
 
 class Broker
   def initialize(portfolio: Portfolio.new)
-    @protfolio = portfolio
+    @portfolio = portfolio
   end
 
   attr_reader :portfolio
 
-  def buy(stock, qty)
-    portfolio.remove_cash(stock.price * qty)
+  def buy(stock, price, qty)
+    portfolio.remove_cash(price * qty)
     portfolio.add_stock(stock, qty)
   rescue => e
     false
   end
 
-  def sell(stock, qty)
+  def sell(stock, price, qty)
     portfolio.remove_stock(stock, qty)
-    portfolio.add_cash(stock.price * qty)
+    portfolio.add_cash(price * qty)
   rescue => e
     false
   end
@@ -76,91 +76,101 @@ Stock = Struct.new(:symbol, :price, :date) do
     symbol == other.symbol && price == other.price && date == other.date
   end
 end
-broker = Broker.new
+
+BROKER = Broker.new
+BUY_THRESHOLD = 95
+SELL_THRESHOLD = 105
 
 kb = RuleRover.knowledge_base(system: :first_order, engine: :backward_chaining) do
-  # assert "stock", "periods", "date", :moving_average_is, "average"
-  # assert "stock", "date", :costs, "price"
-  # assert "stock", :buy, "qty"
-  # assert "stock", :sell,  "qty"
-
-  # NOTE date is in the stock struct
-  # assert "stock", :cross_above, "average"
-  # assert "stock", :cross_below, "average"
-  # assert "stock", :greater_than, "average"
-  # assert "stock", :less_than, "average"
-
-
-  # rule [["stock", "date", :costs, "price"], :and, [:@today, "date"]], :then, [:@calculate_average, :stock] do
-  #   do_action :calculate_average, stock: "stock", date: "date" do |stock:, date:|
-  #     binding.pry
-  #   end
-  # end
-
-  rule [["stock", :greater_than, "average"], :and, ["prevstock", :less_than, "prevaverage"]], :then, ["stock", :cross_above, "average"]
-  rule [["stock", :less_than, "average"], :and, ["prevstock", :greater_than, "prevaverage"]], :then, ["stock", :cross_below, "average"]
-
-  rule ["stock", :cross_above, "average"], :then, ["stock", :buy, "qty"] do
-    do_action :execute_buy_trade, stock: "stock", qty: "qty" do |stock:, qty:|
-      broker.buy(stock, qty)
-    end
+  action :execute_buy_trade do |stock:, qty:, price:|
+    BROKER.buy stock, price, qty
   end
 
-  rule ["stock", :cross_below, "average"], :then, ["stock", :sell, "qty"] do
-    do_action :execute_sell_trade, stock: "stock", qty: "qty" do |stock:, qty:|
-      broker.sell(stock, qty)
-    end
+  action :execute_sell_trade do |stock:, qty:, price:|
+    BROKER.sell stock, price, qty
+  end
+
+  rule [[[:@today, "date"], :and, ["stock", "date", :costs, "price"]], :and, ["price", :below, BUY_THRESHOLD]], :then, ["stock", :buy, "qty"] do
+    do_action :execute_buy_trade, stock: "stock", qty: "qty", price: "price"
+  end
+
+  rule [[[:@today, "date"], :and, ["stock", "date", :costs, "price"]], :and, ["price", :above, SELL_THRESHOLD]], :then, ["stock", :sell, "qty"] do
+    do_action :execute_sell_trade, stock: "stock", qty: "qty", price: "price"
   end
 end
 
-# matches = kb.match?("y", Date.new(2024, 1, 1), :costs, "x")
-# matches = kb.match?(Stock.new("CMG"), Date.new(2024, 1, 1), 65)
-# assert(Stock.new("AAPL", 100, Date.today.strftime("%m-%d-%Y")))
-# assert(Stock.new("AAPL", 101, Date.today.strftime("%m-%d-%Y")))
-# assert(Stock.new("AAPL", 101, Date.today.strftime("%m-%d-%Y")))
+# class Indicator
+#   def initialize(stock, period)
+#     @stock = stock
+#     @period = period
+#   end
+
+#   attr_reader :stock, :period
+
+#   def calculate
+#     moving_average = moving_average(stock, period)
+#   end
+# end
+
+def moving_average(prices, period)
+  prices = prices.last(period)
+  prices.sum(&:price) / prices.size.to_f
+end
+
+class MarketSimulation
+  class << self
+    def run(stock_prices, knowledge_base)
+      new(stock_prices, knowledge_base).run
+    end
+  end
+
+  def initialize(stock_prices, knowledge_base)
+    @stock_prices = stock_prices
+    @knowledge_base = knowledge_base
+  end
+
+  attr_reader :stock_prices, :knowledge_base
+
+  def run
+    # NOTE: main loop
+    stock_prices.each do |stock|
+      puts stock.to_s
+      # STEP: add market data for the day
+      #  - update stock prices
+      #  - update moving averages and other indicators
+
+      curr_date = stock.date
+      knowledge_base.retract :@today, "x"
+      knowledge_base.retract [stock.symbol, "y"], :costs, "x"
+
+      knowledge_base.assert :@today, curr_date.to_s
+      knowledge_base.assert [stock.symbol, stock.date.to_s], :costs, stock.price
+
+      if stock.price > SELL_THRESHOLD
+        knowledge_base.assert stock.price, :above, SELL_THRESHOLD
+        # NOTE: why not just write broker.sell(stock, 1) here?
+        # A more sophisticated strategy would string together many rules
+        # in order to determine when to sell.
+        knowledge_base.entail? stock.symbol, :sell, 10
+      elsif stock.price < BUY_THRESHOLD
+        knowledge_base.assert stock.price, :below, BUY_THRESHOLD
+        # NOTE: why not just write broker.buy(stock, 1) here?
+        knowledge_base.entail? stock.symbol, :buy, 10
+      end
+    end
+  end
+end
 
 stock_updates = [
-  Stock.new("AAPL", 91.0, Date.new(2023, 10, 1)),
-  Stock.new("AAPL", 94.0, Date.new(2023, 11, 1)),
-  Stock.new("AAPL", 98.0, Date.new(2023, 12, 1)),
-  Stock.new("AAPL", 100.0, Date.new(2024, 1, 1)),
-  Stock.new("AAPL", 110.0, Date.new(2024, 2, 1)),
-  Stock.new("AAPL", 107.0, Date.new(2024, 3, 1)),
-  Stock.new("AAPL", 106.0, Date.new(2024, 4, 1)),
-  Stock.new("AAPL", 113.0, Date.new(2024, 5, 1)),
-  Stock.new("AAPL", 112.0, Date.new(2024, 6, 1))
+  Stock.new("ACME", 91.0, Date.new(2023, 10, 1)),
+  Stock.new("ACME", 94.0, Date.new(2023, 11, 1)),
+  Stock.new("ACME", 98.0, Date.new(2023, 12, 1)),
+  Stock.new("ACME", 100.0, Date.new(2024, 1, 1)),
+  Stock.new("ACME", 110.0, Date.new(2024, 2, 1)),
+  Stock.new("ACME", 107.0, Date.new(2024, 3, 1)),
+  Stock.new("ACME", 102.0, Date.new(2024, 4, 1)),
+  Stock.new("ACME", 94.0, Date.new(2024, 5, 1)),
+  Stock.new("ACME", 99.0, Date.new(2024, 6, 1))
 ]
 
-def moving_average(quotes, period, date)
-  quotes = quotes.select { |price| price.date <= date }
-  quotes = quotes.last(period)
-  quotes.sum(&:price) / quotes.size.to_f
-end
-
-stock_updates.each do |stock|
-  current_date = stock.date
-
-  kb.retract :@today, "x"
-  kb.assert :@today, current_date
-
-  kb.assert [stock.symbol, stock.date], :costs, stock.price
-  stocks_prices = kb.match? [stock.symbol, "y"], :costs, "x"
-
-
-  stocks = stocks_prices.map do |stck|
-    ticker_symbol = stck.subjects.find { |subject| subject.name.is_a?(String) }.name
-    quote_date = stck.subjects.find { |subject| subject.name.is_a?(Date) }.name
-    price = stck.objects.find { |object| object.name.is_a?(Float) }.name
-
-    Stock.new(symbol: ticker_symbol, date: quote_date, price: price)
-  end
-
-  moving_avg = moving_average(stocks, 3, current_date)
-
-  kb.assert([stock.symbol, current_date], :moving_average_is, moving_avg)
-
-  kb.entail? stock.symbol, :sell, 10
-  kb.entail? stock.symbol, :buy, 10
-end
-
-moving_avgs = kb.match?("stock", "date", :moving_average_is, "average")
+MarketSimulation.run(stock_updates, kb)
