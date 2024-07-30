2393acf9f0064c4c28271188f5ed41adf9e53876
Joseph Platta <jwplatta@gmail.com>
2024-07-29 21:27:17 -0500

    chore: add some comments for clarity

--- examples/trading_strategy.rb
+++ examples/trading_strategy.rb
@@ -2,6 +2,9 @@ require "pry"
 require "date"
 require "rule_rover"
 
+##################################
+### Trading Simulation Classes ###
+##################################
 class Portfolio
   def initialize(cash: 100_000)
     @stocks = {} # { Stock => quantity }
@@ -72,56 +75,6 @@ Stock = Struct.new(:symbol, :price, :date, :volume, :rsi) do
     symbol == other.symbol && price == other.price && date == other.date && volume == other.volume && rsi == other.rsi
   end
 end
-
-BROKER = Broker.new
-
-kb = RuleRover.knowledge_base(system: :first_order, engine: :backward_chaining) do
-  action :execute_buy_10 do |stock:,  price:|
-    BROKER.buy stock, price, 10
-  end
-
-  action :execute_buy_20 do |stock:,  price:|
-    BROKER.buy stock, price, 20
-  end
-
-  action :execute_sell_10 do |stock:, price:|
-    BROKER.sell stock, price, 10
-  end
-
-  action :execute_sell_20 do |stock:, price:|
-    BROKER.sell stock, price, 20
-  end
-
-  # NOTE: buy signals
-  rule ["stock", :two_std_above, "long_avg"], :then, [:@strong_buy_signal, "stock"]
-  rule ["stock", :one_std_above, "long_avg"], :then, [:@weak_buy_signal, "stock"]
-  rule [[:@weak_buy_signal, "stock"], :and, ["stock", :above_avg_vol, "volume"]], :then, [:@strong_buy_signal, "stock"]
-  rule [[:@weak_buy_signal, "stock"], :and, ["stock", :below_avg_vol, "volume"]], :then, [:@check_rsi, "stock"]
-  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_below_30, "rsi"]], :then, [:@strong_buy_signal, "stock"]
-  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_above_30, "rsi"]], :then, [:@buy_signal, "stock"]
-  rule [[:@strong_buy_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :buy, "20"] do
-    do_action :execute_buy_20, stock: "stock", price: "price"
-  end
-  rule [[:@buy_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :buy, "10"] do
-    do_action :execute_buy_10, stock: "stock", price: "price"
-  end
-
-  # NOTE: sell signals
-  rule ["stock", :two_std_below, "long_avg"], :then, [:@strong_sell_signal, "stock"]
-  rule ["stock", :one_std_below, "long_avg"], :then, [:@weak_sell_signal, "stock"]
-  rule [[:@weak_sell_signal, "stock"], :and, ["stock", :above_avg_vol, "volume"]], :then, [:@strong_sell_signal, "stock"]
-  rule [[:@weak_sell_signal, "stock"], :and, ["stock", :below_avg_vol, "volume"]], :then, [:@check_rsi, "stock"]
-  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_below_70, "rsi"]], :then, [:@strong_sell_signal, "stock"]
-  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_above_70, "rsi"]], :then, [:@sell_signal, "stock"]
-
-  rule [[:@strong_sell_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :sell, 20] do
-    do_action :execute_sell_20, stock: "stock", price: "price"
-  end
-  rule [[:@sell_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :sell, 10] do
-    do_action :execute_sell_10, stock: "stock", price: "price"
-  end
-end
-
 
 class MarketSimulation
   class << self
@@ -221,6 +174,9 @@ class MarketSimulation
   end
 end
 
+###########################
+### EXAMPLE STARTS HERE ###
+###########################
 stock_updates = [
   Stock.new("ACME", 250.00, Date.new(2023, 6, 1), 15000000, 55),
   Stock.new("ACME", 252.00, Date.new(2023, 6, 2), 15500000, 57),
@@ -253,6 +209,56 @@ stock_updates = [
   Stock.new("ACME", 245.00, Date.new(2023, 6, 29), 15600000, 38),
   Stock.new("ACME", 244.00, Date.new(2023, 6, 30), 15550000, 35)
 ]
+
+BROKER = Broker.new
+
+kb = RuleRover.knowledge_base(system: :first_order, engine: :backward_chaining) do
+  action :execute_buy_10 do |stock:,  price:|
+    BROKER.buy stock, price, 10
+  end
+
+  action :execute_buy_20 do |stock:,  price:|
+    BROKER.buy stock, price, 20
+  end
+
+  action :execute_sell_10 do |stock:, price:|
+    BROKER.sell stock, price, 10
+  end
+
+  action :execute_sell_20 do |stock:, price:|
+    BROKER.sell stock, price, 20
+  end
+
+  # NOTE: buy signals
+  rule ["stock", :two_std_above, "long_avg"], :then, [:@strong_buy_signal, "stock"]
+  rule ["stock", :one_std_above, "long_avg"], :then, [:@weak_buy_signal, "stock"]
+  rule [[:@weak_buy_signal, "stock"], :and, ["stock", :above_avg_vol, "volume"]], :then, [:@strong_buy_signal, "stock"]
+  rule [[:@weak_buy_signal, "stock"], :and, ["stock", :below_avg_vol, "volume"]], :then, [:@check_rsi, "stock"]
+  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_below_30, "rsi"]], :then, [:@strong_buy_signal, "stock"]
+  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_above_30, "rsi"]], :then, [:@buy_signal, "stock"]
+  rule [[:@strong_buy_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :buy, "20"] do
+    do_action :execute_buy_20, stock: "stock", price: "price"
+  end
+  rule [[:@buy_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :buy, "10"] do
+    do_action :execute_buy_10, stock: "stock", price: "price"
+  end
+
+  # NOTE: sell signals
+  rule ["stock", :two_std_below, "long_avg"], :then, [:@strong_sell_signal, "stock"]
+  rule ["stock", :one_std_below, "long_avg"], :then, [:@weak_sell_signal, "stock"]
+  rule [[:@weak_sell_signal, "stock"], :and, ["stock", :above_avg_vol, "volume"]], :then, [:@strong_sell_signal, "stock"]
+  rule [[:@weak_sell_signal, "stock"], :and, ["stock", :below_avg_vol, "volume"]], :then, [:@check_rsi, "stock"]
+  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_below_70, "rsi"]], :then, [:@strong_sell_signal, "stock"]
+  rule [[:@check_rsi, "stock"], :and, ["stock", :rsi_above_70, "rsi"]], :then, [:@sell_signal, "stock"]
+
+  rule [[:@strong_sell_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :sell, 20] do
+    do_action :execute_sell_20, stock: "stock", price: "price"
+  end
+  rule [[:@sell_signal, "stock"], :and, ["stock", :costs, "price"]], :then, ["stock", :sell, 10] do
+    do_action :execute_sell_10, stock: "stock", price: "price"
+  end
+end
+
 
 MarketSimulation.run(stock_updates, kb)
 
